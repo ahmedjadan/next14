ccd0c6169cab511a96dbe75ffaa7e5d52eb9a827
ahmedjadan <a.jadan7@gmail.com>
2023-11-29 15:53:05 +0200

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -8,14 +9,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
 
   const params = new URLSearchParams();
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
